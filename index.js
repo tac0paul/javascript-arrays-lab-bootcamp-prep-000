fe20b21ce17136b93e0fe71822c4fac154284e64
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-17 02:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten()
+{
+  pop(0,kittens.length - 1)
+}
